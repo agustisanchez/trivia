f6c90be94bdedf38b468e44cce1f8c07dcefd0f9
agustisanchez <agusti.sanchez@gmail.com>
2016-09-14 04:53:56 +0200

    #2 soporte para tests unitarios

---
+++ java_test/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class GameTest {
+
+	@Test
+	public void emptyTest() {
+	}
+}
