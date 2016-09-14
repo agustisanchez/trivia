2225ce997502c3742bdf9b18ec2c3c3ec3cb83da
agustisanchez <agusti.sanchez@gmail.com>
2016-09-14 05:13:29 +0200

    Test soporte más de 6 usuarios -- se debría haber empezao con el test antes dela refactorización (TDD)

--- java_test/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java_test/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -1,5 +1,7 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.Random;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -8,6 +10,31 @@ import org.junit.runners.JUnit4;
 public class GameTest {
 
 	@Test
-	public void emptyTest() {
+	public void whenMoreThanSixPlayersAreAdded_thenNoError() {
+
+		Game game = new Game();
+		for (int i = 0; i < 10; i++) {
+			game.add("Player " + i);
+		}
+
+		play(game);
+
+	}
+
+	private void play(Game game) {
+		Random rand = new Random();
+
+		boolean notAWinner;
+		do {
+
+			game.roll(rand.nextInt(5) + 1);
+
+			if (rand.nextInt(9) == 7) {
+				notAWinner = game.wrongAnswer();
+			} else {
+				notAWinner = game.wasCorrectlyAnswered();
+			}
+
+		} while (notAWinner);
 	}
 }
